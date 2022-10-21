f7803d98ccc7db041b6470e54531e86125a1a2d4
Nurik <you@example.com>
2022-10-21 14:44:41 +0300

    typing serial numbers

---
+++ Nat_NumbAndEverything_Esle/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Type the Natural number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int n = 2; 
+while(n <= number )
+{
+    Console.Write(n);
+    Console.Write(",");
+    n+=2;
+}
