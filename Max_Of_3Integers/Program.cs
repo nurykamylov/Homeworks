053a063b20f3fc761ab0fed1f928e41a94b3c051
Nurik <you@example.com>
2022-10-21 04:21:36 +0300

    Finding_The_Max_of 3 integers

---
+++ Max_Of_3Integers/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("a = ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("b = ");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.Write("c = ");
+int num3 = int.Parse(Console.ReadLine()!);
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write("max = ");
+Console.WriteLine(max);
