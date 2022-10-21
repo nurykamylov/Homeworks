b11bc773e6b1c88d741e8acf5184d456573b2e07
Nurik <you@example.com>
2022-10-21 03:46:24 +0300

    Finding_the_Max_of_2_int

---
+++ Max_Of_2integer/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("a = ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("b = ");
+int num2 = int.Parse(Console.ReadLine()!);
+if (num1 > num2 )
+{
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+}
+else 
+{
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+}
