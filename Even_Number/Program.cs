13af292a27c5b0b6404866ef5e202ff35e083c9c
Nurik <you@example.com>
2022-10-21 04:56:08 +0300

    Even_numbers added

---
+++ Even_Number/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Type the number: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 2 == 0 ) 
+{
+    Console.WriteLine("Yes, it's even number");
+}
+else
+{
+    Console.WriteLine("No, it's not even number");
+}
